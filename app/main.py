d302bf596120980705b65ff9f387de8f61e95c92
Noureddine El Fatimi <nourddineeagles@gmail.com>
2025-07-07 01:24:18 +0100

    processing with updating operations

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ from sqlalchemy.orm import Session
 from typing import Optional
 from fastapi.responses import FileResponse
 import shemas
-import os
 import random
 import string
+import os
 from pathlib import Path
 
 Base.metadata.create_all(engine)
 
+UPLOAD_PATH = Path(__file__).resolve().parent.parent / "uploads"
 ALLOWED_IMAGE_TYPES = {"image/jpeg", "image/png"}
 ALLOWED_DOC_TYPES = {"application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}
 MAX_FILE_SIZE = 5 * 1024 * 1024 
@@ -33,7 +34,7 @@ def get_by_search(db: Session = Depends(get_db), id_secteur_activite: Optional[i
         offres = offres.filter(models.OffreEmploi.type_offre == type_offre)
     return offres.all()
 
-@app.post("/offres")
+@app.post("/offres", status_code = 201)
 def post_offre(offre: shemas.OffreIn, db: Session = Depends(get_db)):
     entreprise = models.Entreprise(nom_entreprise = offre.nom_entreprise, logo_id = offre.id_fichier, ville_id = offre.ville_id_entreprise, adresse = offre.adresse)
     db.add(entreprise)
@@ -54,7 +55,7 @@ def get_by_id(id_user:int, db: Session = Depends(get_db)):
     offres = db.query(models.OffreEmploi).filter(models.OffreEmploi.id_user == id_user)
     return offres
 
-@app.post("/upload")
+@app.post("/upload", status_code = 201)
 async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     content_type = file.content_type
     if content_type not in ALLOWED_IMAGE_TYPES.union(ALLOWED_DOC_TYPES):
@@ -63,12 +64,10 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
     if len(content) > MAX_FILE_SIZE:
         raise HTTPException(status_code=400, detail="Fichier trop volumineux (max 5 Mo)")
     extension = Path(file.filename).suffix.lower()
-    print(str(Path(__file__)))
-    base_dir = Path(__file__).resolve().parent.parent 
     if content_type in ALLOWED_DOC_TYPES:
-        upload_folder = base_dir / "uploads" / "cv"
+        upload_folder = UPLOAD_PATH / "cv"
     else:
-        upload_folder = base_dir / "uploads" / "images"
+        upload_folder = UPLOAD_PATH / "images"
     upload_folder.mkdir(parents=True, exist_ok=True)
     secure_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=15)) + extension
     full_path = upload_folder / secure_name
@@ -84,14 +83,83 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
     db.refresh(fichier)
     return {"id_fichier": fichier.id_file}
 
+@app.patch("/update-image/{id_entreprise}")
+def update_image(id_entreprise: int, image: shemas.imageUpdate,  db: Session = Depends(get_db)):
+    e = db.query(models.Entreprise).get(id_entreprise)
+    if not e: 
+        raise HTTPException(status_code = 404, detail = "entreprise_inexistant")
+    
+    for k, v in image.model_dump(exclude_defaults = True).items():
+        setattr(e, k, v)
+    return {"message": "success"}
+
+@app.patch("/update/{id_offre}")
+def update(id_offre: int, offre: shemas.OffreUpdate, db: Session = Depends(get_db)):
+    offre_db = db.query(models.OffreEmploi).get(id_offre)
+    if not offre_db: 
+        raise HTTPException(status_code=404, detail="offre_inexistant")
+    update_data = offre.model_dump(exclude_defaults=True)
+    for k, v in update_data.items():
+        if k not in ("entreprise", "competences"):
+            setattr(offre_db, k, v)
+    if "entreprise" in update_data and update_data["entreprise"]:
+        e = offre_db.entreprise
+        for k, v in update_data["entreprise"].items():
+            setattr(e, k, v)
+    if "competences" in update_data and update_data["competences"]:
+        db.query(models.Competence).filter(models.Competence.id_offre == id_offre).delete()
+        for competence in update_data["competences"]:
+            c = models.Competence(
+                nom_competence=competence.nom_competence,
+                niveau=competence.niveau_competence,
+                id_offre=id_offre
+            )
+            db.add(c)
+    db.commit()
+    db.refresh(offre_db)
+    return offre_db
+
 @app.delete("offre/{id_offre}")
 def delete_offre(id_offre: int, db: Session = Depends(get_db)):
     offre = db.query(models.OffreEmploi).get(id_offre)
     if not offre: 
         raise HTTPException(status_code = 404, detail = "offre_inexistant")
+    candidatures = db.query(models.Candidature).filter(models.Candidature.id_offre == offre.id_offre)
+    competences = db.query(models.Competence).filter(models.Competence.id_offre == offre.id_offre)
+    for competence in competences:
+        db.delete(competence)
+    for candidature in candidatures:
+        db.delete(candidature)
     db.delete(offre)
     db.commit()
     return {"message": "offre supprimé"}
+
+@app.delete("/file/{id_file}")
+def delete_file_by_id(id_file: int, db: Session = Depends(get_db)):
+    file = db.query(models.File).get(id_file)
+    if not file:
+        raise HTTPException(status_code = 404, detail = "fichier_inexistant")
+    
+    Path(file.file_path).unlink()
+    db.delete(file)
+    db.commit()
+    return {"message": "fichier_supprimé"}
+
+@app.post("/user", status_code = 201)
+def inscription(user: shemas.UserIn, db: Session = Depends(get_db)):
+    u = models.User(username = user.username, email = user.email, password = user.password, id_role = user.id_role)
+    db.add(u)
+    db.commit()
+    db.refresh(u)
+    return {"message": "user_crée"}
+
+@app.post("/role", status_code = 201)
+def inscription(role: shemas.RoleIn, db: Session = Depends(get_db)):
+    r = models.Role(nom_role = role.nom_role)
+    db.add(r)
+    db.commit()
+    db.refresh(r)
+    return {"message": "role_crée"}
 
 @app.get("/file/{id_file}")
 def get_file_by_id(id_file: int, db: Session = Depends(get_db)):
@@ -107,6 +175,8 @@ def get_by_id(id_offre:int, db: Session = Depends(get_db)):
         raise HTTPException(status_code = 404, detail = "offre_non_trouvé")
     return offre
 
+
+
 @app.get("/refresh_database")
 def refresh(db:Session = Depends(get_db)):
     db.query(models.Candidature).delete()
@@ -120,4 +190,10 @@ def refresh(db:Session = Depends(get_db)):
     db.query(models.File).delete()
     db.query(models.Role).delete()
     db.commit()
+    dir1 = UPLOAD_PATH / "images"
+    dir2 = UPLOAD_PATH / "cv"
+    for file in os.listdir(dir1):
+        os.remove(dir1 / file)
+    for file in os.listdir(dir2):
+        os.remove(dir2 / file)
     return {"message": "Toutes les données ont été supprimées."}
--- app/shemas.py
+++ app/shemas.py
@@ -2,41 +2,50 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+class RoleIn(BaseModel):
+    nom_role: Optional[str]
+
+class UserIn(BaseModel):
+    username: Optional[str]
+    email: Optional[str]
+    password: Optional[str]
+    id_role: Optional[int]
+    
 class UserOut(BaseModel):
-    username: str
+    username: Optional[str]
     
     class Config:
         from_attributes = True
 
 class VilleOut(BaseModel):
-    nom_ville: str
+    nom_ville: Optional[str]
     
     class Config:
         from_attributes = True
 
 class SecteurActiviteOut(BaseModel):
-    nom_secteur: str
+    nom_secteur: Optional[str]
     
     class Config:
         from_attributes = True
 
 class FonctionOut(BaseModel):
-    nom_fonction: str
+    nom_fonction: Optional[str]
     
     class Config:
         from_attributes = True
 
 class FileOut(BaseModel):
-    file_path: str
-    file_name: str
-    file_type: str
+    file_path: Optional[str]
+    file_name: Optional[str]
+    file_type: Optional[str]
 
     class Config:
         from_attributes = True
 
 class EntrepriseOut(BaseModel):
-    nom_entreprise: str
-    adresse: str
+    nom_entreprise: Optional[str]
+    adresse: Optional[str]
     logo: Optional[FileOut]
     ville: Optional[VilleOut]
 
@@ -44,30 +53,30 @@ class EntrepriseOut(BaseModel):
         from_attributes = True
 
 class CompetencesOut(BaseModel):
-    nom_competence: str
-    niveau_competence: str
+    nom_competence: Optional[str]
+    niveau: Optional[str]
 
     class Config:
         from_attributes = True
 
 class OffreOut(BaseModel):
-    titre: str
-    teletravail: str
-    diplome_requis: str
-    niveau_etude_requis: str
-    type_offre: str
-    annee_experience_min: int
-    annee_experience_max: int
-    nbr_employes_demande: int
-    salaire_min: int
-    salaire_max: int
-    description: int
-    nbr_candidats: int
-    created_at: datetime
+    titre: Optional[str]
+    teletravail: Optional[str]
+    diplome_requis: Optional[str]
+    niveau_etude_requis: Optional[str]
+    type_offre: Optional[str]
+    annee_experience_min: Optional[int]
+    annee_experience_max: Optional[int]
+    nbr_employes_demande: Optional[int]
+    salaire_min: Optional[int]
+    salaire_max: Optional[int]
+    description: Optional[str]
+    nbr_de_candidats: Optional[int]
+    created_at: Optional[datetime]
     user: Optional[UserOut]
     entreprise: Optional[EntrepriseOut]
     ville: Optional[VilleOut]
-    secteur_actvite: Optional[SecteurActiviteOut]
+    secteur_activite: Optional[SecteurActiviteOut]
     fonction: Optional[FonctionOut]
     competences: Optional[list[CompetencesOut]]
 
@@ -75,28 +84,59 @@ class OffreOut(BaseModel):
         from_attributes = True
 
 class CompetencesIn(BaseModel):
-    nom_competence: str
-    niveau_competence: str
+    nom_competence: Optional[str]
+    niveau_competence: Optional[str]
 
 class OffreIn(BaseModel):
-    id_fichier:int
-    nom_entreprise: int
-    adresse: int
-    ville_id_entreprise: int
-    id_user: int
-    titre: str
-    teletravail: str
-    diplome_requis: str
-    niveau_etude_requis: str
-    type_offre: str
-    annee_experience_min: int
-    annee_experience_max: int
-    nbr_employes_demande: int
-    salaire_min: int
-    salaire_max: int
-    id_secteur_activite: int
-    id_fonction: int
-    description: str
-    nbr_de_candidats: int
-    id_ville_offre: int
-    competences: list[CompetencesIn]
+    id_fichier: Optional[int] 
+    nom_entreprise: Optional[str] 
+    adresse: Optional[str] 
+    ville_id_entreprise: Optional[int] 
+    id_user: Optional[int] 
+    titre: Optional[str] 
+    teletravail: Optional[str] 
+    diplome_requis: Optional[str] 
+    niveau_etude_requis: Optional[str] 
+    type_offre: Optional[str] 
+    annee_experience_min: Optional[int] 
+    annee_experience_max: Optional[int] 
+    nbr_employes_demande: Optional[int]
+    salaire_min: Optional[int]
+    salaire_max: Optional[int]
+    id_secteur_activite: Optional[int]
+    id_fonction: Optional[int]
+    description: Optional[str]
+    id_ville_offre: Optional[int]
+    competences: Optional[list[CompetencesIn]]
+
+
+class CompetencesUpdate(BaseModel):
+    nom_competence: Optional[str] = None
+    niveau_competence: Optional[str] = None
+
+class imageUpdate(BaseModel):
+    logo_id: int
+
+class EntrepriseUpdate(BaseModel):
+    nom_entreprise: Optional[str] = None
+    adresse: Optional[str] = None
+    id_ville: Optional[int] = None
+
+class OffreUpdate(BaseModel):
+    id_user: Optional[int] = None
+    titre: Optional[str] = None
+    teletravail: Optional[str] = None
+    diplome_requis: Optional[str] = None
+    niveau_etude_requis: Optional[str] = None
+    type_offre: Optional[str] = None
+    annee_experience_min: Optional[int] = None
+    annee_experience_max: Optional[int] = None
+    nbr_employes_demande: Optional[int] = None
+    salaire_min: Optional[int] = None
+    salaire_max: Optional[int] = None
+    id_secteur_activite: Optional[int] = None
+    id_fonction: Optional[int] = None
+    description: Optional[str] = None
+    id_ville: Optional[int] = None
+    entreprise: Optional[EntrepriseUpdate] = None
+    competences: Optional[list[CompetencesUpdate]] = None
